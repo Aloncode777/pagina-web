7bb0f506617d2a6ed6d060137df0ee60514add8e
Aloncode777 <jahm0804@gmail.com>
2025-05-26 13:29:19 -0400

    Add files via upload

---
+++ folklor.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Merida Magica</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<nav class="navbar">
+
+<div>
+
+        <a class="logo" href="index.html">
+          <img src="./img/logo.teleferico.png" alt="logo" width="35px" height="auto" >
+        </a>
+
+</div>
+
+<div>
+
+        <a class="nav-link" href="index.html">Inicio </a>
+
+</div>
+
+<div>
+        <a class="nav-link" href="./nosotros.php">Nosotros</a>
+</div>
+
+<div class="dropdown">
+
+  <input type="checkbox" id="toggle-menu" />
+  <label for="toggle-menu">Más opciones ▾</label>
+
+    <div class="dropdown-menu" role="menu" aria-labelledby="toggle-menu">
+
+      <a href="./cultura.php">Cultura</a>
+      <a href="./tradicion.php">Tradicion</a>
+      <a href="./folklor.php">Folklor</a>
+
+    </div>
+     
+    
+</nav>
+
+<div class="cabecera_img">
+
+<img src="https://ultimasnoticias.com.ve/wp-content/uploads/2021/12/san-benito.jpg" alt="Imagen 1" width="100%" height="500px"/>
+
+<h1 class="titulo_cultura">Folklor Merideño</h1>
+    
+</div>
+
+
+</body>
+</html>
